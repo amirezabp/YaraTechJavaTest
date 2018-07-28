4208394e90da79512c66b787eaa8f824a4f3939b
amirezabp <arfahimibp@gmail.com>
2018-07-28 13:20:43 +0430

    log in page

--- app/src/main/java/com/example/amirreza/myapp/MainActivity.java
+++ app/src/main/java/com/example/amirreza/myapp/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.amirreza.myapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText userName, password;
+    Button signUp, cancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b;
+
+        userName=(EditText) findViewById(R.id.username);
     }
 }
